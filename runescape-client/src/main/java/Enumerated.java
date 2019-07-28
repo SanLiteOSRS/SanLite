26d3d43930901e6cef6ef5a87c59056af9cd4ab7
Juul <->
2019-07-28 21:59:32 +0200

    Even more merge fixes

--- runescape-client/src/main/java/Enumerated.java
+++ runescape-client/src/main/java/Enumerated.java
@@ -1,3 +1,4 @@
+import net.runelite.mapping.Export;
 import net.runelite.mapping.Implements;
 import net.runelite.mapping.ObfuscatedName;
 import net.runelite.mapping.ObfuscatedSignature;
--- runescape-client/src/main/java/class313.java
+++ runescape-client/src/main/java/class313.java
@@ -1,3 +1,4 @@
+import net.runelite.mapping.Export;
 import net.runelite.mapping.ObfuscatedGetter;
 import net.runelite.mapping.ObfuscatedName;
 import net.runelite.mapping.ObfuscatedSignature;
