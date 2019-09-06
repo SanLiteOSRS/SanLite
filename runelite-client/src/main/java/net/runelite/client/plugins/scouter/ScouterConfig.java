d8802266a87f0ed3cd8b90a77b5c793d4ce40bfa
jajack362 <41702061+jajack362@users.noreply.github.com>
2019-09-06 17:13:57 +0100

    Update runelite-client/src/main/java/net/runelite/client/plugins/scouter/ScouterConfig.java
    Co-Authored-By: Juul Damen <31408341+Juuldamen@users.noreply.github.com>

--- runelite-client/src/main/java/net/runelite/client/plugins/scouter/ScouterConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/scouter/ScouterConfig.java
@@ -4,7 +4,7 @@ import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
-@ConfigGroup("scouter")
+@ConfigGroup("playerscouter")
 public interface ScouterConfig extends Config
 {
 	@ConfigItem(
