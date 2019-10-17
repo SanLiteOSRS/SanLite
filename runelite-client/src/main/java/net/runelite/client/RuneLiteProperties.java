3c9c9e56de54c93951cd10b07088283384922b0b
Siraz <->
2019-10-17 20:28:27 +0200

    Changed SanLite icon & launcher version fix test

--- runelite-client/src/main/java/net/runelite/client/RuneLiteProperties.java
+++ runelite-client/src/main/java/net/runelite/client/RuneLiteProperties.java
@@ -40,7 +40,7 @@ public class RuneLiteProperties
 	private static final String GITHUB_LINK = "runelite.github.link";
 	private static final String WIKI_LINK = "runelite.wiki.link";
 	private static final String PATREON_LINK = "runelite.patreon.link";
-	private static final String LAUNCHER_VERSION_PROPERTY = "runelite.launcher.version";
+	private static final String LAUNCHER_VERSION_PROPERTY = "sanlite.launcher.version";
 	private static final String TROUBLESHOOTING_LINK = "runelite.wiki.troubleshooting.link";
 	private static final String BUILDING_LINK = "runelite.wiki.building.link";
 	private static final String DNS_CHANGE_LINK = "runelite.dnschange.link";
