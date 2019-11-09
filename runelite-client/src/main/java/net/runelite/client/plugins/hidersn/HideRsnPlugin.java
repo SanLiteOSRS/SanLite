2edde41cb1b21fee40761e3684c1e9989120c1cb
jajack <jaqthrowaway@gmail.com>
2019-11-09 02:33:51 +0000

    Hide RSN Update
    Removed config as currently no need for it.
    
    Fixed chatbox RSN flickering.

--- runelite-client/src/main/java/net/runelite/client/plugins/hidersn/HideRsnPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/hidersn/HideRsnPlugin.java
@@ -1,20 +1,15 @@
 package net.runelite.client.plugins.hidersn;
 
-import com.google.inject.Provides;
 import com.google.inject.Singleton;
 import net.runelite.api.Client;
-import net.runelite.api.VarClientStr;
-import net.runelite.api.events.ClientTick;
 import net.runelite.api.events.GameTick;
-import net.runelite.api.events.VarClientStrChanged;
+import net.runelite.api.events.ScriptCallbackEvent;
 import net.runelite.api.widgets.Widget;
 import net.runelite.api.widgets.WidgetInfo;
-import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.PluginType;
-import lombok.extern.slf4j.Slf4j;
 
 import javax.inject.Inject;
 
@@ -25,135 +20,74 @@ import javax.inject.Inject;
 		type = PluginType.SANLITE
 )
 
-@Slf4j
 @Singleton
 public class HideRsnPlugin extends Plugin
 {
 	@Inject
 	private Client client;
 
-	@Inject
-	private HideRsnConfig config;
 
-	@Provides
-	HideRsnConfig provideConfig(ConfigManager configManager)
-	{
-		return configManager.getConfig(HideRsnConfig.class);
-	}
-
-
-	/*
 	@Override
 	protected void startUp() throws Exception
 	{
-		if (config.hideRSN())
-		{
-			Widget widget = client.getWidget(WidgetInfo.CHATBOX_INPUT);
-			String[] splitRSN = widget.getText().split(":");
-			splitRSN[0] = "rev-rs.net:";
-			StringBuilder stringBuilder = new StringBuilder();
-			for (int i = 0; i < splitRSN.length; i++)
-			{
-				stringBuilder.append(splitRSN[i]);
-			}
-			String newChatRSN = stringBuilder.toString();
-			widget.setText(newChatRSN);
-		}
+		setRSN(true);
 	}
-	 */
 
-
+	@Override
+	protected void shutDown() throws Exception
+	{
+		setRSN(false);
+	}
 
 	@Subscribe
 	public void onGameTick(GameTick gameTick)
 	{
-		if (config.hideRSN())
-		{
-			Widget widget = client.getWidget(WidgetInfo.CHATBOX_MESSAGE);
-			Widget[] widgetArray = widget.getDynamicChildren();
+		Widget widget = client.getWidget(WidgetInfo.CHATBOX_MESSAGE);
+		Widget[] widgetArray = widget.getDynamicChildren();
 
-			for (int i = 0; i < widgetArray.length; i++)
+		for (int i = 0; i < widgetArray.length; i++)
+		{
+			String[] splitRSN = widgetArray[i].getText().split(":");
+			if (client.isClanMember(splitRSN[0]) || client.getLocalPlayer().getName().equals(splitRSN[0]))
 			{
-				String[] splitRSN = widgetArray[i].getText().split(":");
-				if (client.isClanMember(splitRSN[0]) || client.getLocalPlayer().getName().equals(splitRSN[0]))
-				{
-					widgetArray[i].setText("rev-rs.net:");
-					widgetArray[i + 1].setRelativeX(67);
-				}
+				widgetArray[i].setText("rev-rs.net:");
+				widgetArray[i + 1].setRelativeX(67);
 			}
 		}
 	}
 
-
 	@Subscribe
-	public void onClientTick (ClientTick clientTick)
+	public void onScriptCallbackEvent(ScriptCallbackEvent scriptCallbackEvent)
 	{
-		/*
-		if (config.hideRSN())
+		if (scriptCallbackEvent.getEventName().equals("setChatboxInput"))
 		{
-			Widget widget = client.getWidget(WidgetInfo.CHATBOX_INPUT);
-			String chatboxRSN = widget.getText();
-			String newRSN = chatboxRSN.replace(client.getLocalPlayer().getName(), "rev-rs.net");
-			//log.debug(newRSN);
-			widget.setText(newRSN);
-			widget.revalidate();
-			//log.debug(client.getUsername());
-			//client.setUsername("rev.rs-net");
-
+			setRSN(true);
 		}
+	}
 
-
-
-		if (config.hideRSN())
+	private void setRSN(Boolean custom)
+	{
+		Widget chatboxInput = client.getWidget(WidgetInfo.CHATBOX_INPUT);
+		if (chatboxInput != null)
 		{
-			Widget widget = client.getWidget(WidgetInfo.CHATBOX_INPUT);
-			String[] splitRSN = widget.getText().split(":");
-			splitRSN[0] = "rev-rs.net:";
+			String[] splitRSN = chatboxInput.getText().split(":");
+			if (custom)
+			{
+				splitRSN[0] = "rev-rs.net:";
+			}
+			else
+			{
+				splitRSN[0] = client.getLocalPlayer().getName() + ":";
+			}
 			StringBuilder stringBuilder = new StringBuilder();
+
 			for (int i = 0; i < splitRSN.length; i++)
 			{
 				stringBuilder.append(splitRSN[i]);
 			}
 			String newChatRSN = stringBuilder.toString();
-			widget.setText(newChatRSN);
-		}
 
-		 */
-
-	}
-
-
-
-
-	@Subscribe
-	public void onVarClientStrChanged (VarClientStrChanged varClientStr)
-	{
-		log.debug("event triggered");
-		log.debug(varClientStr + " varClientStr");
-		log.debug(varClientStr.getIndex() + " varClientStr.getIndex()");
-		String index = String.valueOf(varClientStr.getIndex());
-		log.debug(index + " index as string");
-		if (varClientStr.getIndex() == VarClientStr.CHATBOX_TYPED_TEXT.getIndex())
-		{
-			if (config.hideRSN())
-			{
-				log.debug("if passed");
-				Widget widget = client.getWidget(WidgetInfo.CHATBOX_INPUT);
-				String[] splitRSN = widget.getText().split(":");
-				splitRSN[0] = "rev-rs.net:";
-				StringBuilder stringBuilder = new StringBuilder();
-				for (int i = 0; i < splitRSN.length; i++)
-				{
-					stringBuilder.append(splitRSN[i]);
-				}
-				String newChatRSN = stringBuilder.toString();
-				log.debug(newChatRSN + " new chat rsn");
-				widget.setText(newChatRSN);
-				widget.revalidateScroll();
-				widget.revalidate();
-			}
+			chatboxInput.setText(newChatRSN);
 		}
 	}
-
-
 }
