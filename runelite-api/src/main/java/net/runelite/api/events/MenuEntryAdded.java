3b6f1a2c34ca920e8b7a15745621c91f717c81e8
Juul Damen <31408341+Juuldamen@users.noreply.github.com>
2020-01-16 18:28:20 +0100

    Upstream updates (#135)
    * Update to latest upstream
    
    * Add missing changes to world hopper plugin
    
    * Add latest upstream changes
    
    * Add changes from todays game update

--- runelite-api/src/main/java/net/runelite/api/events/MenuEntryAdded.java
+++ runelite-api/src/main/java/net/runelite/api/events/MenuEntryAdded.java
@@ -26,7 +26,6 @@ package net.runelite.api.events;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import net.runelite.api.MenuEntry;
 
 /**
  * An event when a new entry is added to a right-click menu.
@@ -36,63 +35,30 @@ import net.runelite.api.MenuEntry;
 public class MenuEntryAdded
 {
 	/**
-	 * The MenuEntry object that was added
-	 */
-	private final MenuEntry menuEntry;
-
-	/**
 	 * The option text added to the menu (ie. "Walk here", "Use").
 	 */
-	public String getOption()
-	{
-		return menuEntry.getOption();
-	}
-
+	private final String option;
 	/**
 	 * The target of the action (ie. Item or Actor name).
 	 * <p>
 	 * If the option does not apply to any target, this field
 	 * will be set to empty string.
 	 */
-	public String getTarget()
-	{
-		return menuEntry.getTarget();
-	}
-
+	private final String target;
 	/**
 	 * The action type that will be triggered.
 	 */
-	public int getType()
-	{
-		return menuEntry.getType();
-	}
-
+	private final int type;
 	/**
 	 * An identifier value for the target of the action
 	 */
-	public int getIdentifier()
-	{
-		return menuEntry.getIdentifier();
-	}
-
+	private final int identifier;
 	/**
 	 * An additional parameter for the action.
 	 */
-	public int getActionParam0()
-	{
-		return menuEntry.getParam0();
-	}
-
+	private final int actionParam0;
 	/**
 	 * A second additional parameter for the action.
 	 */
-	public int getActionParam1()
-	{
-		return menuEntry.getParam1();
-	}
-
-	public boolean isForceLeftClick()
-	{
-		return menuEntry.isForceLeftClick();
-	}
+	private final int actionParam1;
 }
