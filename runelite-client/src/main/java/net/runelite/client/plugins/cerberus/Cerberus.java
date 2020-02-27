24050c7e8007a6ce2e8d015808c9780a1d247404
jajack362 <41702061+jajack362@users.noreply.github.com>
2020-02-27 19:05:01 +0000

    Cerberus Plugin update (#165)
    * Checks for Cerb Region + early stages of attack order logic
    
    * Auto stash before merge of "feature/cerberus" and "develop"
    
    * Added overhead prayer guides for Cerberus attacks
    
    Prediction seems a bit off sometimes - check animation counting.
    Ghosts prediction not working - test using projectiles from the ghosts instead.
    
    * Corrected cerberus attack counting & ghost style predicting
    
    * Cerberus plugin cleanup
    
    * Fix indenting
    
    * Add config & plugin cleanup
    
    * Version bump & remove color config group

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/cerberus/Cerberus.java
@@ -0,0 +1,261 @@
+package net.runelite.client.plugins.cerberus;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import net.runelite.api.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+class Cerberus
+{
+	enum Attack
+	{
+		DEFAULT,
+		TRIPLE,
+		GHOSTS,
+		LAVA
+	}
+
+	enum AttackStyle
+	{
+		MAGE,
+		RANGE,
+		MELEE
+	}
+
+	@Getter
+	@RequiredArgsConstructor
+	enum CerberusGhost
+	{
+		RANGE(NpcID.SUMMONED_SOUL, Skill.RANGED),
+		MAGE(NpcID.SUMMONED_SOUL_5868, Skill.MAGIC),
+		MELEE(NpcID.SUMMONED_SOUL_5869, Skill.ATTACK);
+
+		private final int npcId;
+		private final Skill type;
+	}
+
+	@Getter
+	@Setter
+	private NPC npc;
+
+	@Getter
+	private AttackStyle currentAttackStyle;
+
+	@Getter
+	@Setter
+	private Attack currentAttack;
+
+	@Getter
+	@Setter
+	private int attackCount;
+
+	@Getter
+	private int health;
+
+	@Getter
+	@Setter
+	private int tripleAttackCount;
+
+	@Getter
+	@Setter
+	private boolean tripleAttack;
+
+	@Getter
+	@Setter
+	private boolean ghostsActive;
+
+	@Getter
+	@Setter
+	private int ghostsCycleCount;
+
+	@Getter
+	@Setter
+	private int timeOfLastAnimation;
+
+	@Getter
+	private final List<NPC> ghosts = new ArrayList<>();
+
+	@Getter
+	private List<GraphicsObject> poolsGraphicObjects = new ArrayList<>();
+
+	Cerberus(NPC npc)
+	{
+		this.npc = npc;
+		this.attackCount = 1;
+		this.currentAttack = Attack.TRIPLE;
+		this.tripleAttackCount = 0;
+		this.tripleAttack = false;
+		this.currentAttackStyle = AttackStyle.MAGE;
+		this.ghostsCycleCount = 0;
+		this.timeOfLastAnimation = 0;
+	}
+
+	boolean isCerberusAttack(int animID)
+	{
+		return animID == AnimationID.CERBERUS_MAGE || animID == AnimationID.CERBERUS_RANGE ||
+				animID == AnimationID.CERBERUS_MELEE || animID == AnimationID.CERBERUS_LAVA ||
+				animID == AnimationID.CERBERUS_GHOSTS;
+	}
+
+	boolean isCerberusGhost(int npcID)
+	{
+		return npcID == CerberusGhost.MAGE.getNpcId() || npcID == CerberusGhost.MELEE.getNpcId() ||
+				npcID == CerberusGhost.RANGE.getNpcId();
+	}
+
+	boolean isGhostProjectile(int projectileID)
+	{
+		return projectileID == ProjectileID.CERBERUS_GHOST_MAGE || projectileID == ProjectileID.CERBERUS_GHOST_RANGE ||
+				projectileID == ProjectileID.CERBERUS_GHOST_MELEE;
+	}
+
+	boolean isGhostsOverheadText()
+	{
+		boolean result = false;
+		if (npc.getOverheadText() != null)
+		{
+			if (npc.getOverheadText().equals("Aaarrrooooooo"))
+			{
+				result = true;
+			}
+		}
+		return result;
+	}
+
+	boolean isCerberusProjectile(int projectileID)
+	{
+		return projectileID == ProjectileID.CERBERUS_MAGE || projectileID == ProjectileID.CERBERUS_RANGE;
+	}
+
+	boolean isPoolGraphicsObject(int graphicsID)
+	{
+		return graphicsID == GraphicID.CERBERUS_POOL;
+	}
+
+	boolean isValidCerberusAttack(int clientTick)
+	{
+		return (clientTick - timeOfLastAnimation) >= 170 || isTripleAttack();
+	}
+
+	void setNextPrayer()
+	{
+		if (ghostsActive && !ghosts.isEmpty())
+		{
+			if (ghosts.get(ghostsCycleCount - 1).getId() == CerberusGhost.MAGE.getNpcId())
+			{
+				currentAttackStyle = AttackStyle.MAGE;
+			}
+			else if (ghosts.get(ghostsCycleCount - 1).getId() == CerberusGhost.RANGE.getNpcId())
+			{
+				currentAttackStyle = AttackStyle.RANGE;
+			}
+			else if (ghosts.get(ghostsCycleCount - 1).getId() == CerberusGhost.MELEE.getNpcId())
+			{
+				currentAttackStyle = AttackStyle.MELEE;
+			}
+		}
+		else if (currentAttack == Attack.DEFAULT)
+		{
+			currentAttackStyle = AttackStyle.MAGE;
+		}
+		else
+		{
+			switch (currentAttack)
+			{
+				case LAVA:
+					currentAttackStyle = AttackStyle.MAGE;
+					break;
+				case TRIPLE:
+					switch (tripleAttackCount)
+					{
+						case 0:
+							currentAttackStyle = AttackStyle.MAGE;
+							break;
+						case 1:
+							currentAttackStyle = AttackStyle.RANGE;
+							break;
+						case 2:
+							currentAttackStyle = AttackStyle.MELEE;
+							break;
+					}
+					break;
+			}
+		}
+	}
+
+	void checkGhostCycle()
+	{
+		if (ghostsCycleCount == 4)
+		{
+			ghostsCycleCount = 0;
+			ghostsActive = false;
+		}
+	}
+
+	void checkPoolSpawns(List<GraphicsObject> graphicsObjects)
+	{
+		poolsGraphicObjects = graphicsObjects.stream()
+				.filter(x -> isPoolGraphicsObject(x.getId()))
+				.collect(Collectors.toList());
+	}
+
+	void updateHealth()
+	{
+		final NPC cerberus = npc;
+		final int lastMaxHealth = 600;
+
+		int lastRatio = 0;
+		int lastHealthScale = 0;
+		if (cerberus.getHealth() > 0)
+		{
+			lastRatio = cerberus.getHealthRatio();
+			lastHealthScale = cerberus.getHealth();
+
+		}
+
+		// Health bar
+		if (lastRatio >= 0 && lastHealthScale > 0)
+		{
+			// This is the reverse of the calculation of healthRatio done by the server
+			// which is: healthRatio = 1 + (healthScale - 1) * health / maxHealth (if health > 0, 0 otherwise)
+			// It's able to recover the exact health if maxHealth <= healthScale.
+			int health = 0;
+			if (lastRatio > 0)
+			{
+				int minHealth = 1;
+				int maxHealth;
+				if (lastHealthScale > 1)
+				{
+					if (lastRatio > 1)
+					{
+						// This doesn't apply if healthRatio = 1, because of the special case in the server calculation that
+						// health = 0 forces healthRatio = 0 instead of the expected healthRatio = 1
+						minHealth = (lastMaxHealth * (lastRatio - 1) + lastHealthScale - 2) / (lastHealthScale - 1);
+					}
+					maxHealth = (lastMaxHealth * lastRatio - 1) / (lastHealthScale - 1);
+					if (maxHealth > lastMaxHealth)
+					{
+						maxHealth = lastMaxHealth;
+					}
+				}
+				else
+				{
+					// If healthScale is 1, healthRatio will always be 1 unless health = 0
+					// so we know nothing about the upper limit except that it can't be higher than maxHealth
+					maxHealth = lastMaxHealth;
+				}
+				// Take the average of min and max possible healths
+				health = (minHealth + maxHealth + 1) / 2;
+			}
+
+			// Show both the hitpoint and percentage values if enabled in the config
+			this.health = health;
+		}
+	}
+}
--- runelite-client/src/main/java/net/runelite/client/plugins/cerberus/CerberusPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/cerberus/CerberusPlugin.java
@@ -26,92 +26,308 @@
 package net.runelite.client.plugins.cerberus;
 
 import com.google.common.collect.ComparisonChain;
-import java.util.ArrayList;
-import java.util.List;
-import javax.inject.Inject;
-import javax.inject.Singleton;
+import com.google.inject.Provides;
 import lombok.Getter;
-import net.runelite.api.GameState;
+import net.runelite.api.AnimationID;
+import net.runelite.api.Client;
 import net.runelite.api.NPC;
-import net.runelite.api.events.GameStateChanged;
-import net.runelite.api.events.GameTick;
-import net.runelite.api.events.NpcDespawned;
-import net.runelite.api.events.NpcSpawned;
+import net.runelite.api.NpcID;
+import net.runelite.api.events.*;
+import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.Subscribe;
+import net.runelite.client.events.ConfigChanged;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.plugins.PluginType;
 import net.runelite.client.ui.overlay.OverlayManager;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+import java.util.ArrayList;
+import java.util.List;
+
 @PluginDescriptor(
-	name = "Cerberus",
-	description = "Show what to pray against the summoned souls",
-	tags = {"bosses", "combat", "ghosts", "prayer", "pve", "overlay", "souls"},
-	type = PluginType.SANLITE_USE_AT_OWN_RISK
+		name = "Cerberus",
+		description = "Show what to pray against Cerberus, including special attacks",
+		tags = {"bosses", "combat", "ghosts", "prayer", "pve", "overlay", "souls", "pvm", "cerberus", "slayer"},
+		type = PluginType.SANLITE_USE_AT_OWN_RISK
 )
 @Singleton
 public class CerberusPlugin extends Plugin
 {
-	@Getter
-	private final List<NPC> ghosts = new ArrayList<>();
+	@Inject
+	private Client client;
 
 	@Inject
 	private OverlayManager overlayManager;
 
 	@Inject
+	private CerberusConfig config;
+
+	@Inject
 	private CerberusOverlay overlay;
+
+	@Inject
+	private CerberusDebugOverlay debugOverlay;
+
+	@Getter
+	private Cerberus cerberus;
+
+	@Getter
+	private boolean inEncounterRegion;
+
+	private final int[] CERBERUS_REGION_WEST = {
+			4626, 4627,
+			4882, 4883,
+			5138, 5139
+	};
+
+	private final int[] CERBERUS_REGION_EAST = {
+			5138, 5139,
+			5349, 5395,
+			5650, 5651
+	};
+
+	private final int[] CERBERUS_REGION_NORTH = {
+			4883, 4884,
+			5139, 5140,
+			5395, 5396
+	};
+
+	private ArrayList<int[]> CERBERUS_REGIONS = new ArrayList<>();
+
+	private boolean isNpcCerberus(int npcID)
+	{
+		return npcID == NpcID.CERBERUS ||
+				npcID == NpcID.CERBERUS_5863 ||
+				npcID == NpcID.CERBERUS_5866;
+	}
+
+	@Provides
+	CerberusConfig getConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(CerberusConfig.class);
+	}
 
 	@Override
 	protected void startUp() throws Exception
 	{
 		overlayManager.add(overlay);
+		if (config.showDebugOverlay())
+		{
+			overlayManager.add(debugOverlay);
+		}
+		inEncounterRegion = false;
+		CERBERUS_REGIONS.add(CERBERUS_REGION_EAST);
+		CERBERUS_REGIONS.add(CERBERUS_REGION_NORTH);
+		CERBERUS_REGIONS.add(CERBERUS_REGION_WEST);
 	}
 
 	@Override
 	protected void shutDown() throws Exception
 	{
 		overlayManager.remove(overlay);
-		ghosts.clear();
+		if (config.showDebugOverlay())
+		{
+			overlayManager.remove(debugOverlay);
+		}
+		inEncounterRegion = false;
 	}
 
 	@Subscribe
-	public void onGameStateChanged(GameStateChanged event)
+	public void onConfigChanged(ConfigChanged event)
 	{
-		GameState gameState = event.getGameState();
-		if (gameState == GameState.LOGIN_SCREEN || gameState == GameState.HOPPING || gameState == GameState.CONNECTION_LOST)
+		if (config.showDebugOverlay())
 		{
-			ghosts.clear();
+			overlayManager.add(debugOverlay);
+		}
+		else if (!config.showDebugOverlay())
+		{
+			overlayManager.remove(debugOverlay);
+		}
+	}
+
+	private boolean isCerberusRegion()
+	{
+		boolean isInRegion = false;
+		for (int[] intArray : CERBERUS_REGIONS)
+		{
+			List<Integer> intListCerbRegion = new ArrayList<>(intArray.length);
+
+			for (int i : intArray)
+			{
+				intListCerbRegion.add(i);
+			}
+
+			List<Integer> intListMapRegion = new ArrayList<>(client.getMapRegions().length);
+
+			for (int i : client.getMapRegions())
+			{
+				intListMapRegion.add(i);
+			}
+
+			isInRegion = intListMapRegion.containsAll(intListCerbRegion);
+		}
+		return isInRegion;
+	}
+
+	@Subscribe
+	protected void onClientTick(ClientTick event)
+	{
+		inEncounterRegion = isCerberusRegion();
+
+		if (validateInstanceAndNpc())
+		{
+			cerberus.updateHealth();
+			cerberus.setNextPrayer();
+			cerberus.checkPoolSpawns(client.getGraphicsObjects());
+
+			if (!cerberus.getGhosts().isEmpty())
+			{
+				cerberus.getGhosts().sort((a, b) -> ComparisonChain.start()
+						.compare(a.getLocalLocation().getX(), b.getLocalLocation().getX())
+						.result());
+
+				if (cerberus.getGhosts().size() > 3)
+				{
+					cerberus.getGhosts().sort((a, b) -> ComparisonChain.start()
+							.compare(a.getLocalLocation().getY(), b.getLocalLocation().getY())
+							.result());
+				}
+			}
+
+			if ((cerberus.getAttackCount() - 1) % 10 == 0)
+			{
+				cerberus.setTripleAttack(true);
+				cerberus.setCurrentAttack(Cerberus.Attack.TRIPLE);
+			}
+			else if (cerberus.getAttackCount() % 7 == 0 && cerberus.getHealth() < 400)
+			{
+				cerberus.setCurrentAttack(Cerberus.Attack.GHOSTS);
+			}
+			else if (cerberus.getAttackCount() % 5 == 0 && cerberus.getHealth() < 200)
+			{
+				cerberus.setCurrentAttack(Cerberus.Attack.LAVA);
+			}
+			else
+			{
+				cerberus.setCurrentAttack(Cerberus.Attack.DEFAULT);
+			}
 		}
 	}
 
 	@Subscribe
 	public void onNpcSpawned(final NpcSpawned event)
 	{
-		final NPC npc = event.getNpc();
-		CerberusGhost.fromNPC(npc).ifPresent(ghost -> ghosts.add(npc));
+		final NPC eventNpc = event.getNpc();
+
+		if (inEncounterRegion && isNpcCerberus(eventNpc.getId()))
+		{
+			cerberus = new Cerberus(eventNpc);
+		}
+		else if (cerberus != null)
+		{
+			if (inEncounterRegion && cerberus.isCerberusGhost(eventNpc.getId()))
+			{
+				cerberus.getGhosts().add(eventNpc);
+			}
+		}
 	}
 
 	@Subscribe
 	public void onNpcDespawned(final NpcDespawned event)
 	{
-		ghosts.remove(event.getNpc());
+		final NPC eventNpc = event.getNpc();
+
+		if (isNpcCerberus(eventNpc.getId()))
+		{
+			cerberus = null;
+		}
+		else if (cerberus != null)
+		{
+			if (cerberus.isCerberusGhost(eventNpc.getId()))
+			{
+				cerberus.getGhosts().remove(eventNpc);
+			}
+		}
+	}
+
+
+	@Subscribe
+	public void onAnimationChanged(AnimationChanged animation)
+	{
+		if (validateInstanceAndNpc())
+		{
+			if (cerberus.isValidCerberusAttack(client.getGameCycle()))
+			{
+				if (cerberus.isCerberusAttack(animation.getActor().getAnimation()))
+				{
+					if (cerberus.isTripleAttack())
+					{
+						if (cerberus.getNpc().getAnimation() == AnimationID.CERBERUS_MELEE)
+						{
+							cerberus.setAttackCount(cerberus.getAttackCount() + 1);
+							cerberus.setTripleAttack(false);
+							cerberus.setTripleAttackCount(0);
+						}
+					}
+					else
+					{
+						cerberus.setAttackCount(cerberus.getAttackCount() + 1);
+					}
+					cerberus.setTimeOfLastAnimation(client.getGameCycle());
+				}
+			}
+		}
 	}
 
 	@Subscribe
-	public void onGameTick(GameTick gameTick)
+	private void onProjectileSpawned(ProjectileSpawned event)
 	{
-		if (ghosts.isEmpty())
+		if (validateInstanceAndNpc())
 		{
-			return;
-		}
+			if (cerberus.isGhostProjectile(event.getProjectile().getId()))
+			{
+				cerberus.setGhostsCycleCount(cerberus.getGhostsCycleCount() + 1);
+				cerberus.checkGhostCycle();
+			}
 
-		ghosts.sort((a, b) -> ComparisonChain.start()
-			// First, sort by the southernmost ghost (e.g with lowest y)
-			.compare(a.getLocalLocation().getY(), b.getLocalLocation().getY())
-			// Then, sort by the westernmost ghost (e.g with lowest x)
-			.compare(a.getLocalLocation().getX(), b.getLocalLocation().getX())
-			// This will give use the current wave and order of the ghosts based on
-			// what ghost will attack first
-			.result());
+			if (cerberus.isValidCerberusAttack(client.getGameCycle()))
+			{
+				if (cerberus.isCerberusProjectile(event.getProjectile().getId()))
+				{
+					if (cerberus.isTripleAttack())
+					{
+						cerberus.setTripleAttackCount(cerberus.getTripleAttackCount() + 1);
+					}
+					else
+					{
+						cerberus.setAttackCount(cerberus.getAttackCount() + 1);
+						cerberus.setTimeOfLastAnimation(client.getGameCycle());
+					}
+				}
+			}
+		}
+	}
+
+	public boolean validateInstanceAndNpc()
+	{
+		return inEncounterRegion && cerberus != null;
+	}
+
+	@Subscribe
+	public void onOverheadTextChanged(OverheadTextChanged event)
+	{
+		if (cerberus != null)
+		{
+			if (event.getActor() == cerberus.getNpc())
+			{
+				if (cerberus.isGhostsOverheadText())
+				{
+					cerberus.setGhostsActive(true);
+					cerberus.setGhostsCycleCount(1);
+				}
+			}
+		}
 	}
 }
