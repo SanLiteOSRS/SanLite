2ee98d517b409f938e51722fe3679c4dba11e965
Juul <31408341+Juuldamen@users.noreply.github.com>
2020-03-05 18:53:50 +0100

    upstream: RuneLite 1.6.9 upgrade (#170)
    * cache: update kit definition
    
    * Use XpTrackerService to determine goals in Agility plugin
    
    * HotColdLocation: Center some location spots
    
    * clues: Improve Yanille dungeon basement clue hint
    
    * itemidentification: add impling jars option
    
    * Add Mythical Cape icon to POH (#10828)
    
    * GroundItems: Keep item lists in input order
    
    * Move matchesSearchTerms util to Text class
    
    * slayer: cancel task when leaving the Inferno
    
    * clues: Update "Slay a nechryael" clue text
    
    * herblore skill calc: add guthix rest
    
    * HotColdLocation: Center South-east Dark Warriors' Fortress location
    
    * mining: add urt, efh, te and basalt respawn timers
    
    * chat commands: Refactor repeated test code into setup method
    
    * chat commands: Add KC reading from POH adventurer's log
    
    * chat commands: Add PB reading from POH adventurer's log
    
    * wintertodt plugin: separate notifications into separate options
    
    * Add screenshot subdirectories
    
    * ground items: change highlight value to take priority over other value thresholds
    
    * tag tabs: respect bank rearrange mode for reordering
    
    * banktags: support viewing all Tag Tabs in the bank screen
    
    * reportbutton: add Game Ticks option
    
    * loot tracker: add drift net reward
    
    * Add AM/PM description to Timestamp Plugin Config (#10920)
    
    * emote clue: Add missing ornament items
    
    * point: lombokize
    
    * tooltips overlay: fix multiple above-cursor tooltips
    
    * item stats: Fix tooltip stab bonus comparison
    
    * Change to use github link instead of email (#10934)
    
    * [Item Charges] Add Amulet of Chemistry and Bounty
    
    * itemcharges: move matchers after message type check
    
    * itemstats: Fix unarmed attack speed
    
    * itemstats: Add tooltip string method tests
    
    * ItemVariations: Add opened variants
    
    * Update Item IDs to 2020-03-05-rev182
    
    * Update Item variations to 2020-03-05-rev182
    
    * Update Object IDs to 2020-03-05-rev182
    
    * Update NPC IDs to 2020-03-05-rev182
    
    * Update Widget IDs to 2020-03-05-rev182
    
    * Update Scripts to 2020-03-05-rev182
    
    * api: update for tinted block and damage hitsplats
    
    * config: fix entering values directly into options with units not working
    
    * demonic gorillas: updated for new hitsplats
    
    * screenshot: Fixed incorrect import
    
    * Version bump 1.6.9 RuneLite, 1.13.1 SanLite
    
    * checkstyle fixes
    
    Co-authored-by: Juul <31408341+Juuldamen@users.noreply.github.com>

--- runelite-client/src/main/java/net/runelite/client/plugins/chatcommands/ChatCommandsPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/chatcommands/ChatCommandsPlugin.java
@@ -25,6 +25,7 @@
  */
 package net.runelite.client.plugins.chatcommands;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.inject.Provides;
 import java.io.IOException;
 import java.util.EnumSet;
@@ -38,12 +39,15 @@ import lombok.extern.slf4j.Slf4j;
 import net.runelite.api.*;
 import net.runelite.api.ItemDefinition;
 import net.runelite.api.events.ChatMessage;
+import net.runelite.api.events.GameStateChanged;
 import net.runelite.api.events.GameTick;
 import net.runelite.api.events.VarbitChanged;
 import net.runelite.api.events.WidgetLoaded;
 import net.runelite.api.vars.AccountType;
 import net.runelite.api.widgets.Widget;
+import static net.runelite.api.widgets.WidgetID.ADVENTURE_LOG_ID;
 import static net.runelite.api.widgets.WidgetID.KILL_LOGS_GROUP_ID;
+import static net.runelite.api.widgets.WidgetID.COUNTERS_LOG_GROUP_ID;
 import net.runelite.api.widgets.WidgetInfo;
 import net.runelite.client.chat.ChatColorType;
 import net.runelite.client.chat.ChatCommandManager;
@@ -57,7 +61,7 @@ import net.runelite.client.input.KeyManager;
 import net.runelite.client.plugins.Plugin;
 import net.runelite.client.plugins.PluginDescriptor;
 import net.runelite.client.util.QuantityFormatter;
-import static net.runelite.client.util.Text.sanitize;
+import net.runelite.client.util.Text;
 import net.runelite.http.api.chat.ChatClient;
 import net.runelite.http.api.chat.Duels;
 import net.runelite.http.api.hiscore.HiscoreClient;
@@ -86,6 +90,9 @@ public class ChatCommandsPlugin extends Plugin
 	private static final Pattern NEW_PB_PATTERN = Pattern.compile("(?i)^(?:Fight |Lap |Challenge |Corrupted challenge )?duration: <col=ff0000>([0-9:]+)</col> \\(new personal best\\)");
 	private static final Pattern DUEL_ARENA_WINS_PATTERN = Pattern.compile("You (were defeated|won)! You have(?: now)? won (\\d+) duels?");
 	private static final Pattern DUEL_ARENA_LOSSES_PATTERN = Pattern.compile("You have(?: now)? lost (\\d+) duels?");
+	private static final Pattern ADVENTURE_LOG_TITLE_PATTERN = Pattern.compile("The Exploits of (.+)");
+	private static final Pattern ADVENTURE_LOG_PB_PATTERN = Pattern.compile("([a-zA-Z]+(?: [a-zA-Z]+)*) Fastest (?:kill|run): ([0-9:]+)");
+
 
 	private static final String TOTAL_LEVEL_COMMAND_STRING = "!total";
 	private static final String PRICE_COMMAND_STRING = "!price";
@@ -101,10 +108,17 @@ public class ChatCommandsPlugin extends Plugin
 	private static final String GC_COMMAND_STRING = "!gc";
 	private static final String DUEL_ARENA_COMMAND = "!duels";
 
+	@VisibleForTesting
+	static final int ADV_LOG_EXPLOITS_TEXT_INDEX = 1;
+
+
 	private final HiscoreClient hiscoreClient = new HiscoreClient();
 	private final ChatClient chatClient = new ChatClient();
 
-	private boolean logKills;
+	private boolean bossLogLoaded;
+	private boolean advLogLoaded;
+	private boolean countersLogLoaded;
+	private String pohOwner;
 	private HiscoreEndpoint hiscoreEndpoint; // hiscore endpoint for current player
 	private String lastBossKill;
 	private int lastPb = -1;
@@ -371,36 +385,65 @@ public class ChatCommandsPlugin extends Plugin
 	@Subscribe
 	public void onGameTick(GameTick event)
 	{
-		if (!logKills)
+		if (client.getLocalPlayer() == null)
 		{
 			return;
 		}
 
-		logKills = false;
-
-		Widget title = client.getWidget(WidgetInfo.KILL_LOG_TITLE);
-		Widget bossMonster = client.getWidget(WidgetInfo.KILL_LOG_MONSTER);
-		Widget bossKills = client.getWidget(WidgetInfo.KILL_LOG_KILLS);
-
-		if (title == null || bossMonster == null || bossKills == null
-				|| !"Boss Kill Log".equals(title.getText()))
+		if (advLogLoaded)
 		{
-			return;
-		}
+			advLogLoaded = false;
 
-		Widget[] bossChildren = bossMonster.getChildren();
-		Widget[] killsChildren = bossKills.getChildren();
-
-		for (int i = 0; i < bossChildren.length; ++i)
-		{
-			Widget boss = bossChildren[i];
-			Widget kill = killsChildren[i];
-
-			String bossName = boss.getText().replace(":", "");
-			int kc = Integer.parseInt(kill.getText().replace(",", ""));
-			if (kc != getKc(bossName))
+			Widget adventureLog = client.getWidget(WidgetInfo.ADVENTURE_LOG);
+			Matcher advLogExploitsText = ADVENTURE_LOG_TITLE_PATTERN.matcher(adventureLog.getChild(ADV_LOG_EXPLOITS_TEXT_INDEX).getText());
+			if (advLogExploitsText.find())
 			{
-				setKc(bossName, kc);
+				pohOwner = advLogExploitsText.group(1);
+			}
+		}
+
+		if (bossLogLoaded && (pohOwner == null || pohOwner.equals(client.getLocalPlayer().getName())))
+		{
+			bossLogLoaded = false;
+
+			Widget title = client.getWidget(WidgetInfo.KILL_LOG_TITLE);
+			Widget bossMonster = client.getWidget(WidgetInfo.KILL_LOG_MONSTER);
+			Widget bossKills = client.getWidget(WidgetInfo.KILL_LOG_KILLS);
+
+			if (title == null || bossMonster == null || bossKills == null
+					|| !"Boss Kill Log".equals(title.getText()))
+			{
+				return;
+			}
+
+			Widget[] bossChildren = bossMonster.getChildren();
+			Widget[] killsChildren = bossKills.getChildren();
+
+			for (int i = 0; i < bossChildren.length; ++i)
+			{
+				Widget boss = bossChildren[i];
+				Widget kill = killsChildren[i];
+
+				String bossName = boss.getText().replace(":", "");
+				int kc = Integer.parseInt(kill.getText().replace(",", ""));
+				if (kc != getKc(longBossName(bossName)))
+				{
+					setKc(longBossName(bossName), kc);
+				}
+			}
+		}
+
+		if (countersLogLoaded && pohOwner.equals(client.getLocalPlayer().getName()))
+		{
+			countersLogLoaded = false;
+
+			String counterText = Text.sanitizeMultilineText(client.getWidget(WidgetInfo.COUNTERS_LOG_TEXT).getText());
+			Matcher mCounterText = ADVENTURE_LOG_PB_PATTERN.matcher(counterText);
+			while (mCounterText.find())
+			{
+				String bossName = mCounterText.group(1);
+				String pbTime = mCounterText.group(2);
+				setPb(longBossName(bossName), timeStringToSeconds(pbTime));
 			}
 		}
 	}
@@ -408,14 +451,29 @@ public class ChatCommandsPlugin extends Plugin
 	@Subscribe
 	public void onWidgetLoaded(WidgetLoaded widget)
 	{
-		// don't load kc if in an instance, if the player is in another players poh
-		// and reading their boss log
-		if (widget.getGroupId() != KILL_LOGS_GROUP_ID || client.isInInstancedRegion())
+		switch (widget.getGroupId())
 		{
-			return;
+			case ADVENTURE_LOG_ID:
+				advLogLoaded = true;
+				break;
+			case KILL_LOGS_GROUP_ID:
+				bossLogLoaded = true;
+				break;
+			case COUNTERS_LOG_GROUP_ID:
+				countersLogLoaded = true;
+				break;
 		}
+	}
 
-		logKills = true;
+	@Subscribe
+	public void onGameStateChanged(GameStateChanged event)
+	{
+		switch (event.getGameState())
+		{
+			case LOADING:
+			case HOPPING:
+				pohOwner = null;
+		}
 	}
 
 	@Subscribe
@@ -478,7 +536,7 @@ public class ChatCommandsPlugin extends Plugin
 		}
 		else
 		{
-			player = sanitize(chatMessage.getName());
+			player = Text.sanitize(chatMessage.getName());
 		}
 
 		search = longBossName(search);
@@ -559,7 +617,7 @@ public class ChatCommandsPlugin extends Plugin
 		}
 		else
 		{
-			player = sanitize(chatMessage.getName());
+			player = Text.sanitize(chatMessage.getName());
 		}
 
 		Duels duels;
@@ -616,7 +674,7 @@ public class ChatCommandsPlugin extends Plugin
 		}
 		else
 		{
-			player = sanitize(chatMessage.getName());
+			player = Text.sanitize(chatMessage.getName());
 		}
 
 		int qp;
@@ -690,7 +748,7 @@ public class ChatCommandsPlugin extends Plugin
 		}
 		else
 		{
-			player = sanitize(chatMessage.getName());
+			player = Text.sanitize(chatMessage.getName());
 		}
 
 		search = longBossName(search);
@@ -773,7 +831,7 @@ public class ChatCommandsPlugin extends Plugin
 		}
 		else
 		{
-			player = sanitize(chatMessage.getName());
+			player = Text.sanitize(chatMessage.getName());
 		}
 
 		int gc;
@@ -983,7 +1041,7 @@ public class ChatCommandsPlugin extends Plugin
 		}
 		else
 		{
-			player = sanitize(chatMessage.getName());
+			player = Text.sanitize(chatMessage.getName());
 		}
 
 		try
@@ -1251,7 +1309,7 @@ public class ChatCommandsPlugin extends Plugin
 	private HiscoreLookup getCorrectLookupFor(final ChatMessage chatMessage)
 	{
 		Player localPlayer = client.getLocalPlayer();
-		final String player = sanitize(chatMessage.getName());
+		final String player = Text.sanitize(chatMessage.getName());
 
 		// If we are sending the message then just use the local hiscore endpoint for the world
 		if (chatMessage.getType().equals(ChatMessageType.PRIVATECHATOUT)
@@ -1381,6 +1439,7 @@ public class ChatCommandsPlugin extends Plugin
 				return "Corporeal Beast";
 
 			case "jad":
+			case "tzhaar fight cave":
 				return "TzTok-Jad";
 
 			case "kq":
@@ -1500,12 +1559,17 @@ public class ChatCommandsPlugin extends Plugin
 			// The Gauntlet
 			case "gaunt":
 			case "gauntlet":
+			case "the gauntlet":
 				return "Gauntlet";
 
 			// Corrupted Gauntlet
 			case "cgaunt":
 			case "cgauntlet":
+			case "the corrupted gauntlet":
 				return "Corrupted Gauntlet";
+
+			case "the nightmare":
+				return "Nightmare";
 
 			default:
 				return WordUtils.capitalize(boss);
