576c7b4d8cb843467d69f38250d97b53a3a1b735
Siraz <51876947+Sirazzz@users.noreply.github.com>
2020-10-08 01:17:42 +0200

    bootstrapper: optimize performance, copy artifacts & refactor (#230)
    * bootstrapper: generate file checksums efficiently
    
    * bootstrapper: copy dynamic artifacts & refactor
    
    Co-authored-by: Siraz <->

--- bootstrapper/src/main/java/net/runelite/bootstrap/Bootstrapper.java
+++ bootstrapper/src/main/java/net/runelite/bootstrap/Bootstrapper.java
@@ -6,9 +6,17 @@ import com.google.gson.GsonBuilder;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Bootstrapper
 {
+
+	public static Map<DynamicArtifact, String> CHECKSUMS;
 
 	public static void main(String[] args)
 	{
@@ -25,15 +33,19 @@ public class Bootstrapper
 		}
 
 		// Create output directory
-		File outputDir = new File("./bootstrapper/target/bootstrap-output");
+		File outputDir = new File("./target/bootstrap-output");
 		if (!outputDir.exists())
 		{
-			if (!outputDir.mkdir())
+			if (!outputDir.mkdirs())
 			{
 				System.err.println("Could not create output directory");
 				System.exit(1);
 			}
 		}
+
+		// Generate dynamic dependency file checksums
+		System.out.println("Generating file checksums for commit: " + buildCommit);
+		CHECKSUMS = generateFileChecksums();
 
 		// Generate bootstrap files for all types
 		for (BootstrapType bootstrapType : BootstrapType.values())
@@ -44,27 +56,71 @@ public class Bootstrapper
 
 	private static void generateBootstrap(BootstrapType type, String buildCommit)
 	{
-		System.out.println("Generating " + type.getName() + " bootstrap file | commit: " + buildCommit);
+		System.out.println("Generating " + type.getName() + " bootstrap file");
 		Gson gson = new GsonBuilder().disableHtmlEscaping().setPrettyPrinting().create();
 
-		File bootstrapDir = new File("./bootstrapper/target/bootstrap-output/" + type.getOutputDir());
+		File bootstrapDir = new File("./target/bootstrap-output/" + type.getOutputDir());
 		if (!bootstrapDir.exists())
 		{
-			if (!bootstrapDir.mkdir())
+			if (!bootstrapDir.mkdirs())
 			{
 				System.err.println("Could not create output directory: " + type.getOutputDir() + " for bootstrap file: " + type.getName());
 				System.exit(1);
 			}
 		}
 
+		copyDynamicArtifacts(bootstrapDir);
+
 		try (FileWriter fileWriter = new FileWriter(bootstrapDir.getPath() + "/bootstrap.json"))
 		{
-			gson.toJson(new Bootstrap(type.getRepositoryUrl(), buildCommit), fileWriter);
+			gson.toJson(new Bootstrap(type.getRepositoryUrl(), buildCommit, CHECKSUMS), fileWriter);
 		}
 		catch (IOException | IllegalArgumentException e)
 		{
 			System.err.println("Unable to generate " + type.getName() + " bootstrap file");
 			e.printStackTrace();
 		}
+	}
+
+	private static void copyDynamicArtifacts(File outputDir)
+	{
+		for (DynamicArtifact artifact : DynamicArtifact.values())
+		{
+			try
+			{
+				System.out.println("Copying " + artifact.getFile() + " to " + outputDir);
+				Files.copy(Paths.get(artifact.getFile().getCanonicalPath()),
+						Paths.get(new File(outputDir + "/" + artifact.getFileName()).getCanonicalPath()),
+						StandardCopyOption.REPLACE_EXISTING);
+			}
+			catch (IOException e)
+			{
+				System.err.println("Unable to copy artifact for " + artifact.getName());
+				e.printStackTrace();
+			}
+		}
+	}
+
+	static Map<DynamicArtifact, String> generateFileChecksums()
+	{
+		Map<DynamicArtifact, String> checksums = new HashMap<>();
+		for (DynamicArtifact dynamicArtifact : DynamicArtifact.values())
+		{
+			try
+			{
+				checksums.put(dynamicArtifact, FileChecksum.getChecksumFile(
+						"../" + dynamicArtifact.getDirectory() +
+								"/target/" + dynamicArtifact.getName() + "-" +
+								new BootstrapperProperties().getRuneLiteVersion() + ".jar"));
+			}
+			catch (IOException | NoSuchAlgorithmException e)
+			{
+				System.err.println("Unable to generate file checksum for " + dynamicArtifact);
+				e.printStackTrace();
+				System.exit(1);
+			}
+		}
+
+		return checksums;
 	}
 }
---
+++ bootstrapper/src/main/java/net/runelite/bootstrap/DynamicArtifact.java
@@ -0,0 +1,32 @@
+package net.runelite.bootstrap;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.io.File;
+
+@AllArgsConstructor
+public enum DynamicArtifact
+{
+	RUNELITE_CLIENT("runelite-client", "client"),
+	RUNELITE_API("runelite-api", "runelite-api"),
+	RUNESCAPE_API("runescape-api", "runescape-api"),
+	HTTP_API("http-api", "http-api"),
+	INJECTED_CLIENT("injected-client", "injected-client");
+
+	@Getter
+	private final String directory;
+
+	@Getter
+	private final String name;
+
+	public File getFile()
+	{
+		return new File("../" + getDirectory() + "/target/" + getFileName());
+	}
+
+	public String getFileName()
+	{
+		return getName() + "-" + new BootstrapperProperties().getRuneLiteVersion() + ".jar";
+	}
+}
