6cd16b1fc515d3b8d66a020dfb1661cb2a250287
Siraz <->
2021-05-29 16:16:59 +0200

    feat(rs-api): adds var transmit listener getter

--- runescape-api/src/main/java/net/runelite/rs/api/RSWidget.java
+++ runescape-api/src/main/java/net/runelite/rs/api/RSWidget.java
@@ -584,6 +584,10 @@ public interface RSWidget extends Widget
 
 	@Import("onVarTransmit")
 	@Override
+	Object[] getOnVarTransmitListener();
+
+	@Import("onVarTransmit")
+	@Override
 	void setOnVarTransmitListener(Object[] o);
 
 	@Import("fillMode")
