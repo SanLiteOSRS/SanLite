c74711f13ce0ec34ec8ac131cd90d8adbf8ba14d
Siraz <->
2021-06-16 22:25:35 +0200

    fix(rs-client): re-adds mouse wheel

---
+++ runescape-client/src/main/java/MouseWheel.java
@@ -0,0 +1,17 @@
+import net.runelite.mapping.Export;
+import net.runelite.mapping.Implements;
+import net.runelite.mapping.ObfuscatedName;
+import net.runelite.mapping.ObfuscatedSignature;
+
+@ObfuscatedName("ei")
+@Implements("MouseWheel")
+public interface MouseWheel
+{
+	@ObfuscatedName("v")
+	@ObfuscatedSignature(
+		descriptor = "(B)I",
+		garbageValue = "-39"
+	)
+	@Export("useRotation")
+	int useRotation();
+}
