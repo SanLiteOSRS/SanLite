66c39c2228446dd42bd80ca454225ed7922272bb
Siraz <->
2021-08-25 18:31:04 +0200

    feat: bumps vanilla rs-client version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -29,7 +29,7 @@ object ProjectVersions {
 
     const val sanliteVersion = "1.20.1"
 
-    const val rsVersion = 198
+    const val rsVersion = 199
     const val cacheVersion = 165
 
     const val lombokVersion = "1.18.20"
