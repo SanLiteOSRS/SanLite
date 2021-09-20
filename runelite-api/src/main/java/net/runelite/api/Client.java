0ce9aa2f4d29b331e9a8d320efb413b6eaf61359
Adam <Adam@sigterm.info>
2021-09-20 10:39:52 -0400

    api: remove cachedModels2 and nodecache setters
    This is being moved internally since it benefits areas like the farming guild even with gpu off

--- runelite-api/src/main/java/net/runelite/api/Client.java
+++ runelite-api/src/main/java/net/runelite/api/Client.java
@@ -1790,8 +1790,6 @@ public interface Client extends GameEngine
 
 	TextureProvider getTextureProvider();
 
-	NodeCache getCachedModels2();
-
 	void setRenderArea(boolean[][] renderArea);
 
 	int getRasterizer3D_clipMidX2();
