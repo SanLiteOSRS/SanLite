d098fa42defd2fad3f29ab90bb6666781f4c673b
Siraz <->
2021-10-20 23:11:18 +0200

    feat: api fixes
    cba atm

--- runelite-client/src/main/java/net/runelite/client/util/GameEventManager.java
+++ runelite-client/src/main/java/net/runelite/client/util/GameEventManager.java
@@ -30,16 +30,8 @@ import java.util.Optional;
 import java.util.function.Consumer;
 import javax.inject.Inject;
 import javax.inject.Singleton;
-import net.runelite.api.Client;
-import net.runelite.api.Constants;
-import net.runelite.api.GameState;
-import net.runelite.api.ItemContainer;
-import net.runelite.api.NPC;
-import net.runelite.api.Node;
-import net.runelite.api.Player;
-import net.runelite.api.Scene;
-import net.runelite.api.Tile;
-import net.runelite.api.TileItem;
+
+import net.runelite.api.*;
 import net.runelite.api.events.DecorativeObjectSpawned;
 import net.runelite.api.events.GameObjectSpawned;
 import net.runelite.api.events.GroundObjectSpawned;
@@ -111,9 +103,15 @@ public class GameEventManager
 
 			eventBus.register(subscriber);
 
-			for (final ItemContainer itemContainer : client.getItemContainers())
+			// TODO: 870933985da8d9a12cda586c42fe237651d4afc0
+			for (final InventoryID inventory : InventoryID.values())
 			{
-				eventBus.post(new ItemContainerChanged(itemContainer.getId(), itemContainer));
+				final ItemContainer itemContainer = client.getItemContainer(inventory);
+
+				if (itemContainer != null)
+				{
+					eventBus.post(new ItemContainerChanged(inventory.getId(), itemContainer));
+				}
 			}
 
 			for (NPC npc : client.getCachedNPCs())
