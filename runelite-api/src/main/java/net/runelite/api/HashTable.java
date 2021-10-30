5de67dc38eeb46bc57b3011aebdb52ddc8c8dba1
Siraz <->
2021-10-30 18:04:55 +0200

    api: make HashTable implement iterable

--- runelite-api/src/main/java/net/runelite/api/HashTable.java
+++ runelite-api/src/main/java/net/runelite/api/HashTable.java
@@ -28,7 +28,7 @@ package net.runelite.api;
  * A data structure that uses a hash function to compute an index into an
  * array of buckets from which node objects can be quickly obtained.
  */
-public interface HashTable<T extends Node>
+public interface HashTable<T extends Node> extends Iterable<T>
 {
 	/**
 	 * Gets a node by its hash value.
