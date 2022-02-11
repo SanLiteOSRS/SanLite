3d7b73c96454b84f5e14c2368734c4a2afd498df
Siraz <->
2022-02-11 19:45:24 +0100

    chore: bumps version

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -27,7 +27,7 @@ object ProjectVersions {
     const val launcherVersion = "2.0.0"
     const val rlVersion = "1.8.11"
 
-    const val sanliteVersion = "1.20.20"
+    const val sanliteVersion = "1.20.21"
 
     const val rsVersion = 203
     const val cacheVersion = 165
