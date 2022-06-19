0503c2ed292a48387715a04c4688439a7ade171b
Adam <Adam@sigterm.info>
2022-06-18 22:28:41 -0400

    Add rtconfig for excluded dead npcs

--- runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/menuentryswapper/MenuEntrySwapperPlugin.java
@@ -194,6 +194,9 @@ public class MenuEntrySwapperPlugin extends Plugin
 	@Inject
 	private ChatMessageManager chatMessageManager;
 
+	@Inject
+	private NpcUtil npcUtil;
+
 	private boolean configuringShiftClick = false;
 	private boolean configuringLeftClick = false;
 
@@ -1318,7 +1321,7 @@ public class MenuEntrySwapperPlugin extends Plugin
 			.filter(e ->
 			{
 				final NPC npc = e.getNpc();
-				return npc == null || !NpcUtil.isDying(npc);
+				return npc == null || !npcUtil.isDying(npc);
 			})
 			.toArray(MenuEntry[]::new);
 		if (oldEntries.length != newEntries.length)
