fef8f061297230bf23d54481fa0a1fed0924c048
SanLite CI <->
2023-01-26 11:48:45 +0000

    feat(gamepack): updates to revision 210

---
+++ runescape-client/src/main/java/class30.java
@@ -0,0 +1,33 @@
+import net.runelite.mapping.ObfuscatedName;
+import net.runelite.mapping.ObfuscatedSignature;
+
+@ObfuscatedName("av")
+public interface class30 {
+	@ObfuscatedName("h")
+	@ObfuscatedSignature(
+		descriptor = "(II)Z",
+		garbageValue = "1119176744"
+	)
+	boolean vmethod4138(int var1);
+
+	@ObfuscatedName("e")
+	@ObfuscatedSignature(
+		descriptor = "(II)Z",
+		garbageValue = "-916991313"
+	)
+	boolean vmethod4099(int var1);
+
+	@ObfuscatedName("v")
+	@ObfuscatedSignature(
+		descriptor = "(CI)Z",
+		garbageValue = "-258182"
+	)
+	boolean vmethod4097(char var1);
+
+	@ObfuscatedName("x")
+	@ObfuscatedSignature(
+		descriptor = "(ZI)Z",
+		garbageValue = "333814410"
+	)
+	boolean vmethod4102(boolean var1);
+}
